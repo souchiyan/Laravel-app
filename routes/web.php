0d243f7b4adf8cc86c80ec1c94b12e2c39b68309
souchiyan <souchiyan98@gmail.com>
2024-10-15 12:03:13 +0900

    create calender

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,16 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+Route::get('/', function(){
+    return Inertia::render('Index');
 });
 
 Route::get('/dashboard', function () {
