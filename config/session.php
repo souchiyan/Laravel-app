736f47311dc529e8c8f0cb934f3169af6b423eb7
souchiyan <souchiyan98@gmail.com>
2024-11-28 21:49:54 +0900

    fix session

--- config/session.php
+++ config/session.php
@@ -130,7 +130,7 @@ return [
         'SESSION_COOKIE',
         Str::slug(env('APP_NAME', 'laravel'), '_') . '_session'
     ),
-
+    'admin_cookie' => env('ADMIN_SESSION_COOKIE', 'admin_session'),
     /*
     |--------------------------------------------------------------------------
     | Session Cookie Path
