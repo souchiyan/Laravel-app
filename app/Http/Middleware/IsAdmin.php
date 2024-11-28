3446b46dae3f6b2d2898b16a2b188ea6b776540b
souchiyan <souchiyan98@gmail.com>
2024-11-28 20:08:11 +0900

    fix csrf

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -12,11 +12,22 @@ class IsAdmin
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
+    // public function handle($request, Closure $next)
+    // {
+    //     if (Auth::check() && Auth::admin()->is_admin) {
+    //         return $next($request);
+    //     }
+    //     return redirect('/'); // 管理者でない場合
+    // }
     public function handle($request, Closure $next)
-    {
-        if (Auth::check() && Auth::admin()->is_admin) {
-            return $next($request);
-        }
-        return redirect('/'); // 管理者でない場合
+{
+    if ($request->is('admin/*')) {
+        config(['session.cookie' => config('session.admin_cookie')]);
+    } else {
+        config(['session.cookie' => config('session.user_cookie')]);
     }
+
+    return $next($request);
+}
+
 }
